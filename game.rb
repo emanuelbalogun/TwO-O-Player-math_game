949320c8a98a74aa0864c8b87808571b95e0903c
Emmanuel Balogun <emanuelbalogun@gmail.com>
2023-10-11 00:54:17 -0500

    Implement game helper functions

--- game.rb
+++ game.rb
@@ -1,19 +1,37 @@
+require_relative 'player'
 class Game
-  attr_accessor :player1 , :player2, :new_game_started
+  attr_accessor :player1 , :player2, :new_game_is_started
 def initialize
-  puts "playe1 please input your name"
+  puts "player1 please input your name"
   player1_name = gets.chomp
   @player1 = Player.new(player1_name,true)
   puts "playe2 please input your name"
   player2_name = gets.chomp
   @player2 = Player.new(player2_name,false)
-  @new_game_started = 1
+  @new_game_is_started = 1
 end
 
 def gameover
    (player1.lives == 0) || (player2.lives == 0)
 end
 
+def winner
+  if(player2.lives == 0) ? player1 : player2;
+end
 
+def changeplayer
+ player1.player_turn = !player1.player_turn;
+ player2.player_turn = !player2.player_turn;
+end
+  
+
+def play_game
+  while !gameover
+    if new_game_is_tarted == 1
+      self.new_game_started = 0 
+    else
+      puts "----------  NEW TURN  ----------"
+    end
+end
 
 end
